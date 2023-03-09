c48f61878bc4f8cdba66e24a4c850694fd7dd7c1
jricardoayala1355 <127373402+jricardoayala1355@users.noreply.github.com>
2023-03-09 14:35:59 +0900

    Create bikez.js

---
+++ bikez.js
@@ -0,0 +1,14 @@
+'strict'
+
+
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 200) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+
